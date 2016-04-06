d1ac3984fb4b549e399795cc103c9731655f719b
Kofi Amoussou <karsoft92@gmail.com>
2016-04-06 07:32:20 +0000

    more css forms, removed borders for input

--- contact.php
+++ contact.php
@@ -4,9 +4,9 @@
 	
 			<div class="containter">
 				<div class="jumbo-text">
-					<h1>About Casa Ghana</h1>
+					<h1>Contact</h1>
 					<p>
-						Learn about our goals are and what we are offering you as services.
+						Proudly developed in Ghana. We're here if you want to talk!
 					</p>
 				</div>
 			</div>
@@ -15,23 +15,49 @@
 
 <section class="container">
 <section class="col-sm-6 col-md-6">
-							<h3>Free Real Estate Listings</h3>
-							<article>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi vitae quos natus numquam vero illum eligendi libero praesentium nihil eaque porro placeat molestias, veritatis cumque facilis similique voluptatibus laudantium et earum! Tenetur vero perspiciatis eum quis, totam temporibus explicabo dolorum nesciunt. Impedit, tempore.</article>
+						
+							<article>
+								<h3 class="page-header">Contact us</h3>
+								<ul>
+									<li> <span class="fa fa-phone"></span> 0233 540 920 984 </li>
+									<li><a href="mailto:info@casaghana.com" target="_blank"><span class="fa fa-envelope"></span>&nbsp;info@casaghana.com</a></li>
+									<li><a href="#" target="_blank"></a></li>
+									<ul class="list-inline">
+										<li><a href="#"  target="_blank"><span class="fa fa-facebook"></span></a></li>
+										<li><a href="#" target="_blank"><span class="fa fa-twitter"></span></a></li>
+									</ul>
+								</ul>
+							</article>
 						</section> <!--/service1 -->
 			
 						<section class="col-sm-6 col-md-6">
-							<h3>Individual Accounts</h3>
-							<article>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi vitae quos natus numquam vero illum eligendi libero praesentium nihil eaque porro placeat molestias, veritatis cumque facilis similique voluptatibus laudantium et earum! Tenetur vero perspiciatis eum quis, totam temporibus explicabo dolorum nesciunt. Impedit, tempore.</article>
+						<h3 class="page-header">Contact Form</h3>
+							<form action="" method="" >
+								
+								<section class="form-group">
+										<label for="">Name</label>
+								<input type="" name="" placeholder="" class="form-control"></section>
+
+								<section class="form-group">
+											<label for="">Email</label>
+								<input type="" name="" placeholder="" class="form-control"></section>
+
+								<section class="form-group">
+									<label for="">subject</label>
+								<input type="" name="" placeholder="" class="form-control"></section>
+								<section class="form-group">
+									<label for="">Message</label>
+								<textarea name="" id="" cols="30" rows="10" class="form-control"></textarea></section>
+									<section class="col-md-12 form-group"></section>
+		
+			<section class="col-md-12 form-group"> <button class="btn btn-success form-control">Post</button></section>
+	
+
+
+
+							</form>
 						</section> <!--/service2 -->
-						<section class="col-sm-6 col-md-6">
-							<h3>Nation Wide Ads</h3>
-							<article>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi vitae quos natus numquam vero illum eligendi libero praesentium nihil eaque porro placeat molestias, veritatis cumque facilis similique voluptatibus laudantium et earum! Tenetur vero perspiciatis eum quis, totam temporibus explicabo dolorum nesciunt. Impedit, tempore.</article>
-						</section> <!--/service3 -->
-			
-						<section class="col-sm-6 col-md-6">
-							<h3>Social Media Listing Sharing</h3>
-							<article>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi vitae quos natus numquam vero illum eligendi libero praesentium nihil eaque porro placeat molestias, veritatis cumque facilis similique voluptatibus laudantium et earum! Tenetur vero perspiciatis eum quis, totam temporibus explicabo dolorum nesciunt. Impedit, tempore.</article>
-						</section> <!--/service4 -->
+					
 </section>
 
 	
