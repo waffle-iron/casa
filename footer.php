e372dbc433535f20712dd82de433906bf2c051a6
Kofi Amoussou <karsoft92@gmail.com>
2016-04-13 06:52:17 +0000

    added flexslider to project

--- footer.php
+++ footer.php
@@ -27,9 +27,14 @@
 </footer>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-  <script src="js/jquery-1.11.0.min.js" type="text/javascript"></script>
+ 
+  <script src="bower_components/jquery/dist/jquery.min.js" type="text/javascript"></script>
    <!-- Include all compiled plugins (below), or include individual files as needed -->
+
+   <!-- flexslider jquery -->
+
+  <script src="bower_components/flexslider/jquery.flexslider-min.js" type="text/javascript"></script>
+
     <script src="js/bootstrap.min.js"></script>
     <script  src="js/custom.js"></script>
 
---
+++ js/custom.js
@@ -0,0 +1,6 @@
+    //flexslider
+     $(window).load(function() {
+              $('.flexslider').flexslider({
+                animation: "slide"
+              });
+            });
