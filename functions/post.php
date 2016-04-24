b083d3acedf25da515fdf173365accbbc32ab536
Kofi Amoussou <karsoft92@gmail.com>
2016-04-24 11:14:15 +0000

    fixes

--- functions/post.php
+++ functions/post.php
@@ -16,8 +16,8 @@
 
   $listid=uniqid(rand(0,10000));
   $target_path="images/";
-$target_path=$target_path.basename(implode(',',$_FILES['uploadedfile']['name']));
-if(move_uploaded_file(implode(',',$_FILES['uploadedfile']['tmp_name']), $target_path)) {
+$target_path=$target_path.basename($_FILES['uploadedfile']['name']);
+if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
 
 }
 
